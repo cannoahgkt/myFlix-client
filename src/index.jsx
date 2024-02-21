672cbe41c4feefc858b5b03cda9642feccde52d1
Can Noah <87754848+Asareff@users.noreply.github.com>
2024-02-21 11:43:51 +0100

    3.9 commit

--- src/index.jsx
+++ src/index.jsx
@@ -28,9 +28,24 @@ const MyFlixApplication = () => {
   );
 };
 
+const cors = require('cors');
+let allowedOrigins = ['myflixonline.netlify.app', 'http://localhost:8080'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 // Finds the root of your app
 const container = document.querySelector("#root");
 const root = ReactDOM.createRoot(container);
 
 // Tells React to render your app in the root DOM element
 root.render(<MyFlixApplication />);
+
