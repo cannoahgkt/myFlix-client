99e44e947db7236b864992c43cbe399fd54a574d
Can Noah <87754848+Asareff@users.noreply.github.com>
2024-02-24 12:00:42 +0100

    3.9 commits

--- src/components/nav-bar/navigation-bar.jsx
+++ src/components/nav-bar/navigation-bar.jsx
@@ -13,7 +13,7 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/mainview">Home</Nav.Link>
             
             {user ? (
               <>
