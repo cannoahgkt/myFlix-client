3d84a458e41839153e39888cf116a5fd1e697851
Can Noah <87754848+Asareff@users.noreply.github.com>
2024-03-09 21:48:19 +0100

    login commit

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -14,7 +14,7 @@ export const LoginView = ({ onLoggedIn }) => {
       password: password
     };
     
-    fetch("https://cfmovies-ffc8e49a7be5.herokuapp.com", {
+    fetch("https://cfmovies-ffc8e49a7be5.herokuapp.com/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
